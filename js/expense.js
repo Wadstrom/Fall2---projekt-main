1944d298cec7edce8db01f16b0790f6ea41c0dbf
Tobias Persson <tobiasp89tp@gmail.com>
2021-02-16 14:34:14 +0100

    Tog bort knapp i Expense

--- js/expense.js
+++ js/expense.js
@@ -74,46 +74,5 @@ const getExpenseData = () => {
     });
 };
 
-let inputid = document.getElementById("inputID");
-const getExpenseDataByID = () => {
-  fetch("https://localhost:44357/api/expense/" + inputid.value)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      let output = ` <tr id="expenseTableData">
-            <th>Name:</th>
-            <th>Category:</th>
-            <th>Transaction date:</th>
-            <th>Amount (kr):</th>
-            <th>Expense ID:</th>
-            <th>User ID:</th>
-        </tr>`;
-      data.forEach(function (expense) {
-        // += betyder Append och `` betyder "template-strings där vi kan ha en massor html"
-        output += `
-                <tr>
-                <td>${expense.ExpenseName}</td>            
-                <td>${expense.Category}</td>
-                <td>${new Date(expense.TransactionDate).toDateString()}</td>  
-                <td>${expense.ExpenseAmount}</td>
-                <td>${expense.ExpenseID}</td>
-                <td>${expense.UserID}</td>
-            </tr>
-                `;
-      });
-      document.getElementById("expenseTable").innerHTML = output;
-    });
-};
-//denna  listar i console retur värdena med namn.
-const getExpenseConsole = () => {
-  fetch("https://localhost:44357/api/expense")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => console.log(data));
-};
 
-// function updateTable() {
-//     $('#expenseTableDiv').load(document.URL +  ' #expenseTableDiv');
-// }
+
