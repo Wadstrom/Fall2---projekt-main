cf0ff2dc40cfb4c95856797838313d048409772c
Emil <neanderemil@gmail.com>
2021-03-05 12:07:52 +0100

    social and friend updated

--- js/friend.js
+++ js/friend.js
@@ -1,39 +1,32 @@
-
-// import cookieUserID from "./cookiecutter";
-
-
+import cookieUserID from "./cookiecutter.js";
 
 forms.onsubmit = (e) => {
-    e.preventDefault();
-    console.log(e);
+  e.preventDefault();
+  console.log(e);
 
-    let requestObject = {
-        From_ID: 1,
-        To_ID: e.target[0].value,
-        Status: 0
-    };
+  let requestObject = {
+    From_ID: cookieUserID,
+    To_Email: e.target[0].value,
+    Status: "Pending",
+  };
 
-    fetch("https://localhost:44357/api/friend", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(requestObject),
+  fetch("https://localhost:44357/api/friend", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestObject),
+  });
+};
+
+const getPendingFriend = () => {
+  fetch("https://localhost:44357/api/friend/" + cookieUserID)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
     });
 };
 
-
-const getPendingFriend = () => {
-    fetch(`https://localhost:44357/api/friend/`)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-          console.log(data)
-          
-        });
-        
-};
-
-
-getPendingFriend()
+getPendingFriend();
