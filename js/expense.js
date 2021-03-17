4b8d3aa1b9d1904ff7b3762b1052c3fc37eae581
Wadstrom <boneblooder@hotmail.com>
2021-03-17 14:21:07 +0100

    Fixa lite

--- js/expense.js
+++ js/expense.js
@@ -1,5 +1,5 @@
 import cookieUserID from "./cookiecutter.js";
-
+import generateTable from "./tableGenerator.js"
 forms.onsubmit = (e) => {
   e.preventDefault();
   console.log(e);
@@ -27,25 +27,13 @@ const getExpenseData = () => {
       return response.json();
     })
     .then((data) => {
-      let output = ` <tr id="expenseTableData">
-            <th>Name:</th>
-            <th>Category:</th>
-            <th>Transaction date:</th>
-            <th>Amount (kr):</th>
-        </tr>`;
-      data.forEach(function (expense) {
-        output += `
-                <tr>
-                <td>${expense.ExpenseName}</td>            
-                <td>${expense.Category}</td>
-                <td>${new Date(expense.TransactionDate).toDateString()}</td>  
-                <td>${expense.ExpenseAmount}</td>
-            </tr>
-                `;
+      data.forEach((item) => {
+        item.Date = item.Date.slice(0, 10);
       });
-      document.getElementById("expenseTable").innerHTML = output;
-      });
+      generateTable(data);
+    });
 };
+      
 getExpenseData()
 
 
