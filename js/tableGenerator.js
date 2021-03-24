ef0ad8de44a88bff9ea9f283556b68dc97b1fd7c
Emil <emil.neander@gmail.com>
2021-03-24 10:28:54 +0100

    namechanges + logs

--- js/tableGenerator.js
+++ js/tableGenerator.js
@@ -1,33 +1,41 @@
 //-----------------------------------------
 const generateTable = (data) => {
+  console.log("data: ");
   console.log(data);
   // get the reference for the body
   var body = document.getElementById("table-div");
   // create table and table body
   var tbl = document.createElement("table");
   var tblBody = document.createElement("tbody");
-  //Declare the prop keys from data - "[0]" because we know the prop key won't change
-  //propkey is gonna be the table headers
-  var propKey = Object.keys(data[0]);
+
+  //Declare the object keys from data - "[0]" because we know the obj keys won't change
+  //objKey is gonna be the table headers
+  var objKey = Object.keys(data[0]);
+  console.log("objKey: ");
+  console.log(objKey);
   //copying and adding the first data object to beginning
   data.unshift(data[0]);
+
   // ROWS (one row = r)
   for (var r = 0; r < data.length; r++) {
     var row = document.createElement("tr");
-    //Declare the prop value from data
-    var propValue = Object.values(data[r]);
+    //Declare the obj value from data
+    var objValue = Object.values(data[r]);
+    console.log("objValue:");
+    console.log(objValue);
+
     //COLUMNS (one column = c)
-    for (var c = 0; c < propKey.length; c++) {
+    for (var c = 0; c < objKey.length; c++) {
       var td = document.createElement("td");
       var th = document.createElement("th");
       //if row === 0 then write out th
       if (r === 0) {
-        var headerText = document.createTextNode(propKey[c]);
+        var headerText = document.createTextNode(objKey[c]);
         th.appendChild(headerText);
         row.appendChild(th);
         //else write out all td
       } else {
-        var cellText = document.createTextNode(propValue[c]);
+        var cellText = document.createTextNode(objValue[c]);
         td.appendChild(cellText);
         row.appendChild(td);
       }
